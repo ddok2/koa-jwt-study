4f2a58277854bee801c10bddd6bd5b43fbcdf8f6
SUNGYUB <darkerkorean@gmail.com>
2019-03-21 10:40:31 +0900

    koa-jwt-study-7 Mongoose 통해 MongoDB 연동 데이터 생성과 조회 구현

--- src/api/books/books.controller.js
+++ src/api/books/books.controller.js
@@ -1,9 +1,63 @@
+const Book = require('models/book');
+
 module.exports = {
-    list: (ctx) => {
-        ctx.body = 'listed';
+    get: async ctx => {
+        const { id } = ctx.params;
+        let book;
+
+        try {
+            book = await Book.findById(id).exec();
+        } catch (err) {
+            if (err.name === 'CastError') {
+                ctx.status = 400;
+                return;
+            }
+            return ctx.throw(500, err);
+        }
+
+        if (!book) {
+            ctx.status = 404;
+            ctx.body = { message: 'book not found' };
+            return;
+        }
+        ctx.body = book;
     },
-    create: (ctx) => {
-        ctx.body = 'created';
+    list: async ctx => {
+        let books;
+
+        try {
+            books = await Book.find().sort({ _id: -1 }).limit(3).exec();
+        } catch (err) {
+            return ctx.throw(500, err);
+        }
+
+        ctx.body = books;
+
+    },
+    create: async ctx => {
+        const {
+            title,
+            authors,
+            publishedDdate,
+            price,
+            tags,
+        } = ctx.request.body;
+
+        const book = new Book({
+            title,
+            authors,
+            publishedDdate,
+            price,
+            tags,
+        });
+
+        try {
+            await book.save();
+        } catch (err) {
+            return ctx.throw(500, err);
+        }
+
+        ctx.body = book;
     },
     delete: (ctx) => {
         ctx.body = 'deleted';
@@ -13,5 +67,5 @@ module.exports = {
     },
     update: (ctx) => {
         ctx.body = 'updated';
-    }
+    },
 };
--- src/index.js
+++ src/index.js
@@ -12,7 +12,10 @@ const bodyParser = require('koa-bodyparser');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGO_URI).then(() => {
+mongoose.connect(
+    process.env.MONGO_URI,
+    { useNewUrlParser: true },
+).then(() => {
     console.log(`Successfully connected to MongoDB`);
 }).catch(err => {
     console.error(err);
@@ -25,7 +28,6 @@ app.use(bodyParser());
 router.use('/api', api.routes());
 
 app.use(router.routes()).use(router.allowedMethods());
-
 app.listen(PORT, () => {
     console.log(`Server is listening to port: ${PORT}`);
 });
