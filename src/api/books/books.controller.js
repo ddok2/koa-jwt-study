26b93f29baa39dbb60c3da9e164b603ae6bb7da1
SUNGYUB NA <darkerkorean@gmail.com>
2019-03-22 09:13:17 +0900

    koa-jwt-study-7 Mongoose 통해 MongoDB 연동 데이터 삭제와 수정, 그리고 요청 검증

--- src/api/books/books.controller.js
+++ src/api/books/books.controller.js
@@ -1,3 +1,6 @@
+const Joi = require('joi');
+const { Types: { ObjectId } } = require('mongoose');
+
 const Book = require('models/book');
 
 module.exports = {
@@ -59,13 +62,77 @@ module.exports = {
 
         ctx.body = book;
     },
-    delete: (ctx) => {
-        ctx.body = 'deleted';
+    delete: async ctx => {
+        const { id } = ctx.params;
+
+        try {
+            await Book.findByIdAndRemove(id).exec();
+        } catch (err) {
+            if (err.name === 'CastError') {
+                ctx.status = 400;
+                return;
+            }
+        }
+        ctx.status = 204;
     },
-    replace: (ctx) => {
-        ctx.body = 'replaced';
+    replace: async ctx => {
+        const { id } = ctx.params;
+
+        if (!ObjectId.isValid(id)) {
+            ctx.status = 400;
+            return;
+        }
+
+        const schema = Joi.object().keys({
+            title: Joi.string().required(),
+            authors: Joi.array().items(Joi.object().keys({
+                name: Joi.string().required(),
+                email: Joi.string().email().required(),
+            })),
+            publishedDdate: Joi.date().required(),
+            price: Joi.number().required(),
+            tags: Joi.array().items((Joi.string()).required()),
+        });
+
+        const result = Joi.validate(ctx.request.body, schema);
+
+        if (result.error) {
+            ctx.status = 400;
+            ctx.body = result.error;
+            return;
+        }
+
+        let book;
+
+        try {
+            book = await Book.findByIdAndUpdate(id, ctx.request.body, {
+                upsert: true,
+                new: true,
+            });
+        } catch (err) {
+            return ctx.throw(500, err);
+        }
+
+        ctx.body = book;
     },
-    update: (ctx) => {
-        ctx.body = 'updated';
+    update: async ctx => {
+        const { id } = ctx.params;
+
+        if (!ObjectId.isValid(id)) {
+            ctx.status = 400;
+            return;
+        }
+
+        let book;
+
+        try {
+            book = await Book.findByIdAndUpdate(id, ctx.request.body, {
+                new: true
+            });
+        } catch (e) {
+            return ctx.throw(500, e);
+        }
+
+        ctx.body = book;
     },
 };
